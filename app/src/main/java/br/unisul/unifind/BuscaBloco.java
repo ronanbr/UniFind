9b6c2d41214b03f4c9bdd3c24090604e1fa0b96e
ronan.cardoso <ronan.uni@gmail.com>
2015-10-26 16:06:08 -0200

    Novas Telas

---
+++ app/src/main/java/br/unisul/unifind/BuscaBloco.java
@@ -0,0 +1,13 @@
+package br.unisul.unifind;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuscaBloco extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_busca_bloco);
+    }
+}
